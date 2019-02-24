54c496bd16d36fb283a3dcfe8a5218f95c697ad1
Balazs Szerencsi <balazs.szerencsi@icloud.com>
2019-02-24 18:39:42 +0100

    Randomize tests and added the ability to mark specific tests for execution

--- spec/kong_king_spec.rb
+++ spec/kong_king_spec.rb
@@ -1,9 +1,5 @@
-RSpec.describe KongKing do
+describe KongKing do
   it 'has a version number' do
     expect(KongKing::VERSION).not_to be nil
-  end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
   end
 end
